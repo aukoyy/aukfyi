f2832feb118c1ff6019482c23052a08febb77755
aukoyy <22494963+aukoyy@users.noreply.github.com>
2022-02-09 13:34:46 +0100

    add text types to tidbits

--- studio/schemas/objects/tidbitPortableText.js
+++ studio/schemas/objects/tidbitPortableText.js
@@ -6,12 +6,20 @@ export default {
 		{
 			title: 'Block',
 			type: 'block',
-			styles: [{ title: 'Normal', value: 'normal' }],
-			lists: [],
+			styles: [
+				{ title: 'Normal', value: 'normal' },
+				{ title: 'H1', value: 'h1' },
+				{ title: 'H2', value: 'h2' },
+				{ title: 'H3', value: 'h3' },
+				{ title: 'H4', value: 'h4' },
+				{ title: 'Quote', value: 'blockquote' },
+			],
+			lists: [{ title: 'Bullet', value: 'bullet' }],
 			marks: {
 				decorators: [
 					{ title: 'Strong', value: 'strong' },
 					{ title: 'Emphasis', value: 'em' },
+					{ title: 'Code', value: 'code' },
 				],
 				annotations: [],
 			},
